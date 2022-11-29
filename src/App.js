bc0a0ecc83745c15e0c93fa03802e6971d6d4689
keieng <105902831+keieng@users.noreply.github.com>
2022-11-29 21:02:35 +0900

    表示件数のlimit変更機能追加

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { NavigationBar } from "./components/NavigationBar";
-import { Pagination, Spinner } from "react-bootstrap";
+import { Form, Pagination, Spinner } from "react-bootstrap";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -17,6 +17,8 @@ function App() {
   const [endURL, setEndURL] = useState("");
   const [dataCount, setDataCount] = useState(0);
   const [pageCount, setPageCount] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [offset, setOffset] = useState("");
 
   const fetchPokemonData = async (pageURL) => {
     setLoading(true);
@@ -25,17 +27,26 @@ function App() {
     // 各ポケモンの詳細なデータを取得
     await loadPokemon(res.results);
     setDataCount(res.count);
-    setPageCount(Math.floor(dataCount / 20));
-    const endOffset = pageCount * 20;
-    setEndURL(`${initialURL}?offset=${endOffset}&limit=20`);
+    setPageCount(Math.floor(dataCount / limit));
+    const endOffset = pageCount * limit;
+    setEndURL(`${initialURL}?offset=${endOffset}&limit=${limit}`);
     setNextURL(res.next);
     setPrevURL(res.previous);
     setLoading(false);
   };
 
+  // const offsetUrl = () => {
+  //   setEndURL(`${initialURL}?offset=${offset}&limit=${limit}`);
+  // };
+
+  const changeLimit = (value) => {
+    if (!value) return;
+    setLimit(value);
+  };
+
   useEffect(() => {
-    fetchPokemonData(initialURL);
-  }, []);
+    fetchPokemonData(`${initialURL}?limit=${limit}`);
+  }, [limit]);
 
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
@@ -57,7 +68,42 @@ function App() {
         </h1>
       ) : (
         <Container>
-          <Row sm={4} className="g-4 my-3">
+          <Row className="my-3">
+            <Form.Label className="col-sm-2 col-form-label" htmlFor="limit">
+              表示件数
+            </Form.Label>
+            <Form.Select
+              className="col-sm"
+              id="limit"
+              onChange={(e) => {
+                changeLimit(e.target.value);
+              }}
+            >
+              <option selected={limit == 5} value={5}>
+                5
+              </option>
+              <option selected={limit == 10} value={10}>
+                10
+              </option>
+              <option selected={limit == 20} value={20}>
+                20
+              </option>
+              <option selected={limit == 40} value={40}>
+                40
+              </option>
+              <option selected={limit == 60} value={60}>
+                60
+              </option>
+              <option selected={limit == 80} value={80}>
+                80
+              </option>{" "}
+              <option selected={limit == 80} value={100}>
+                100
+              </option>
+            </Form.Select>
+          </Row>
+
+          <Row sm={5} className="g-4 my-3">
             {pokemonData.map((pokemon) => {
               return (
                 <Col>
