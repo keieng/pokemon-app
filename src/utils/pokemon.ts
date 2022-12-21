cfd80cc0d1aa9059fc1793d19a478844f6b0be46
keieng <105902831+keieng@users.noreply.github.com>
2022-12-21 23:13:30 +0900

    ts,tsxに変更

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,17 +1,17 @@
-export const getAllPokemon = (url) => {
+export const getAllPokemon = (url: string) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => resolve(data));
   });
 };
-export const getAllPokemonAsync = async (url) => {
+export const getAllPokemonAsync = async (url: string) => {
   const res = await fetch(url);
   const data = await res.json();
   return data;
 };
 
-export const getPokemon = (url) => {
+export const getPokemon = (url: string) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
